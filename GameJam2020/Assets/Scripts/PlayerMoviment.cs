f3d97999bbabdb5ded53cfc324b68b4402fd6f6f
Victor Hugo <victorsa.br@gmail.com>
2020-10-10 16:31:07 -0400

    movimentação no personagem

---
+++ GameJam2020/Assets/Scripts/PlayerMoviment.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class PlayerMoviment : MonoBehaviour
+{
+
+    float Speed;
+    void Start()
+    {
+        Speed = 5;
+    }
+
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal") * Time.deltaTime * Speed, 0, Input.GetAxis("Vertical") * Time.deltaTime * Speed);
+    }
+}
