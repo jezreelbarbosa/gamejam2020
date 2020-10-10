a753c3315ad9fb09b70215b8b5f75a4f34702cc9
Victor Hugo <victorsa.br@gmail.com>
2020-10-10 19:46:56 -0400

    yes!!

--- GameJam2020/Assets/Scripts/PlayerMoviment.cs
+++ GameJam2020/Assets/Scripts/PlayerMoviment.cs
@@ -4,32 +4,15 @@ using System.Threading;
 using UnityEngine;
 
 public class PlayerMoviment : MonoBehaviour
-{
-    public Animator animator; 
-
-    public float speed;
-
-    private float _xAxis;
-    private float _zAxis;
-    private Rigidbody _rb;
-
+{    
+    public Animator anima;
     void Start()
     {
-        _rb = GetComponent<Rigidbody>();   
+    
     }
-
     void Update()
     {
-    
-        animator.SetFloat("vertical",Input.GetAxis("Vertical"));
-        animator.SetFloat("horizontal",Input.GetAxis("Horizontal"));
-
-        _xAxis = Input.GetAxis("Horizontal");
-        _zAxis = Input.GetAxis("Vertical");
-    }
-
-    private void FixedUpdate()
-    {
-        _rb.MovePosition(transform.position + Time.deltaTime * speed * transform.TransformDirection(_xAxis, 0f, _zAxis));
+        anima.SetFloat("Vertical",Input.GetAxis("vertical"));
+        anima.SetFloat("Horizontal",Input.GetAxis("Horizontal"));
     }
 }
