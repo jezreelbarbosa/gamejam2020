4a24f4fcead4506885afa34abc24032c35360ca9
Victor Hugo <victorsa.br@gmail.com>
2020-10-11 12:22:22 -0400

    main scene adicionada

--- GameJam2020/Assets/Scripts/PlayerMoviment.cs
+++ GameJam2020/Assets/Scripts/PlayerMoviment.cs
@@ -5,16 +5,25 @@ using UnityEngine;
 
 public class PlayerMoviment : MonoBehaviour
 {    
+    float h = Input.GetAxis("Horizontal");
+    float v = Input.GetAxis("Vertical");
+    public float speed = 5f;
     public Animator anima;
-    void Start()
-    {
-    
-    }
-    void Update()
-    {
-        anima.SetBool("run",Input.GetButton("Vertical")||Input.GetButton("Horizontal"));
-        Debug.Log(Input.GetButton("Vertical"));
-        anima.SetBool("carry",Input.GetKey(KeyCode.Space));
+    Rigidbody rb;
 
+ 
+    void Start () {
+    rb = GetComponent<Rigidbody>();
     }
+    void FixedUpdate () {
+    float mH = Input.GetAxis ("Horizontal");
+    float mV = Input.GetAxis ("Vertical");
+    rb.velocity = new Vector3 (mH * speed, rb.velocity.y, mV * speed);
+    }
+    //void Update()
+    //{
+    //    transform.position = new Vector3(h, 0, v) * speed * Time.deltaTime;
+    //    anima.SetBool("run",Input.GetButton("Vertical")||Input.GetButton("Horizontal")); 
+    //    anima.SetBool("carry",Input.GetKey(KeyCode.Space));        
+    //}
 }
