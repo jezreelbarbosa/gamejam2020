128b9cbd256999374e3b6705734aff6b75387c24
Jezreel Barbosa <jezreel.barbosa.42@gmail.com>
2020-10-11 11:02:16 -0400

    Colisor

--- GameJam2020/Assets/Scripts/CameraController.cs
+++ GameJam2020/Assets/Scripts/CameraController.cs
@@ -10,33 +10,22 @@ public class CameraController : MonoBehaviour
 
     private Rigidbody rb;
 
-    // Use this for initialization
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    //void Update()
-    //{
-
-    //}
-
     void FixedUpdate()
     {
         Vector3 fromCameraToMe = transform.position - mainCamera.transform.position;
-        fromCameraToMe.y = 0; // First, zero out any vertical component, so the movement plane is always horizontal.
-        fromCameraToMe.Normalize(); // Then, normalize the vector to length 1 so that we don't affect the player more strongly when the camera is at different distances.
+        fromCameraToMe.y = 0;
+        fromCameraToMe.Normalize();
                                    
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        //Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        // We can cheat a bit here - we had to flatten out the 'forward' vector from the camera to the player, but the camera is always horizontal left-right, so we can just use
-        // its 'transform.right' vector to determine the direction to move the ball. Add the horizontal and vertical vectors to determine ground-plane movement.
         Vector3 movement = (fromCameraToMe * moveVertical + mainCamera.transform.right * moveHorizontal);
 
-        //rb.velocity = movement * Time.deltaTime * speed;
         rb.MovePosition(transform.position + Time.deltaTime * speed * movement);
         if (movement != Vector3.zero)
         {
