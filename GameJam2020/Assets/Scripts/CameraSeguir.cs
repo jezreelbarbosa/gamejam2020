8a45f27a2dfef94eba6062c2c906476493bc397a
Geovane Leandro da Costa Junior <geovaneleandro.3d@gmail.com>
2020-10-11 17:36:23 -0400

    Script Camera Seguir esta Funcionando

---
+++ GameJam2020/Assets/Scripts/CameraSeguir.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSeguir : MonoBehaviour
+{
+    // Start is called before the first frame update
+    	public GameObject target;
+	public float moveSpeed;
+	void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	if (target != null){
+		transform.position = Vector3.Lerp(transform.position, target.transform.position, Time.deltaTime * moveSpeed);
+	}     
+    }
+}
