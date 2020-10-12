de23c042844b6b4435a1ab5fe863a8d3882f9d00
Jezreel Barbosa <jezreel.barbosa.42@gmail.com>
2020-10-11 23:08:14 -0400

    Monster Death

---
+++ GameJam2020/Assets/Scripts/MonsterDeath.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterDeath : MonoBehaviour
+{
+    public float speed = 1;
+
+    private HealthPoints healthPoints;
+    private bool isDestroyTriggered = false;
+    
+    void Start()
+    {
+        healthPoints = GetComponent<HealthPoints>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (healthPoints.isDead)
+        {
+            if (!isDestroyTriggered)
+            {
+                isDestroyTriggered = true;
+                Destroy(gameObject, 5);
+            }
+
+            transform.position = transform.position + new Vector3(0, -speed * Time.deltaTime, 0);
+        }
+    }
+}
