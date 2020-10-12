b4d79adfff4b1453e92cacbc1da9086203e7e70e
Jezreel Barbosa <jezreel.barbosa.42@gmail.com>
2020-10-11 22:19:22 -0400

    Health Points and damage

---
+++ GameJam2020/Assets/Scripts/HealthPoints.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPoints : MonoBehaviour
+{
+    public int points = 1;
+    public bool isDead = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Damageble damageble = collision.collider.GetComponent<Damageble>();
+        points -= damageble.damage;
+
+        if (points <= 0)
+        {
+            isDead = true;
+        }
+    }
+}
