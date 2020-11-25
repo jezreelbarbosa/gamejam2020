fcd30d8f017c5da69ee7a46af8a3bf16fb416fc4
Victor Hugo <victorsa.br@gmail.com>
2020-11-24 22:24:20 -0400

    adção da movimentação no cubo da cena teste

--- Sails of Sea/Assets/Scripts/MovimentPlayer.cs
+++ Sails of Sea/Assets/Scripts/MovimentPlayer.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class MovimentPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public CharacterController controller;
+
+    public float speed = 6f;
+    
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
+    
     void Update()
-    {
-        
+    {        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal,0f,vertical).normalized;
+        if (direction.magnitude >= 0.1f){
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            controller.Move(direction *speed * Time.deltaTime );
+        }
     }
 }
